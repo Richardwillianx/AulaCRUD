68dbf65feeb57f92e3e9d032d51a2cdb98a974c2
RichardWillianx <richardwilliansp@gmail.com>
2023-04-02 18:30:51 -0300

    inclusão de dados teste

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,22 @@ import {
   updateStudent,
 } from "./features/students";
 
-createStudent();
+createStudent({
+  id: "01",
+  name: "Richard William",
+  cpf: "123.456.789-10",
+  registration: "01020-3",
+});
 
-searchForID();
+createStudent({
+  id: "02",
+  name: "Pedro da Silva",
+  cpf: "098.765.432-10",
+  registration: "02030-4",
+});
 
-updateStudent();
+searchForID("01");
 
-deleteStudent();
+updateStudent("1", { cpf: "157.155.171-10", name: "Hebert Richard" });
+
+deleteStudent("02");
